559df83367e1b117b8f88ab269fe3d8bb5b5b0de
Danjie Wenren <danjie.wenren@gmail.com>
2025-11-02 11:02:16 -0800

    Implement softmax.

--- cs336_basics/functions.py
+++ cs336_basics/functions.py
@@ -1,3 +1,5 @@
+"""Functions."""
+
 import torch
 
 from jaxtyping import Float
@@ -6,3 +8,11 @@ from jaxtyping import Float
 def silu(x: Float[torch.Tensor, "..."]):
     """Runs SiLU."""
     return x * torch.sigmoid(x)
+
+
+def softmax(x: Float[torch.Tensor, "..."], dim: int) -> Float[torch.Tensor, "..."]:
+    """Runs softmax."""
+    x_diffed = x - x.max(dim=dim, keepdim=True)[0]
+    x_exped = torch.exp(x_diffed)
+    x_exped_sumed = torch.sum(x_exped, dim=dim, keepdim=True)
+    return x_exped / x_exped_sumed
--- tests/adapters.py
+++ tests/adapters.py
@@ -10,6 +10,7 @@ from jaxtyping import Bool, Float, Int
 from torch import Tensor
 from cs336_basics.bpe_tokenizer import BpeTokenizer
 from cs336_basics.functions import silu
+from cs336_basics.functions import softmax
 from cs336_basics.layers import Embedding
 from cs336_basics.layers import Linear
 from cs336_basics.layers import RMSNorm
@@ -447,7 +448,7 @@ def run_softmax(in_features: Float[Tensor, " ..."], dim: int) -> Float[Tensor, "
         Float[Tensor, "..."]: Tensor of with the same shape as `in_features` with the output of
         softmax normalizing the specified `dim`.
     """
-    raise NotImplementedError
+    return softmax(in_features, dim)
 
 
 def run_cross_entropy(
